69e86be8b6568a34f503e56fe592466197a786b7
Kodkungen <55358222+Kodkungen@users.noreply.github.com>
2020-06-13 23:06:32 +0200

    Implementing photoresistor

--- main.py
+++ main.py
@@ -12,16 +12,28 @@ import time
 import pycom
 import machine
 from machine import PWM
+from machine import ADC
 from dth import DTH
 from machine import Pin
 from onewire import DS18X20
 from onewire import OneWire
 
-pycom.heartbeat(False)
-pycom.rgbled(0x000008) # blue
-th = DTH(Pin('P3', mode=Pin.OPEN_DRAIN),0)
-pycom.rgbled(0x00FF00)  # Green
-time.sleep(0.5)
+adc = ADC()
+apin = adc.channel(pin='P16', attn=ADC.ATTN_11DB)
+
+def reading():
+   avg = 0
+   _AVG_NUM = const(100)
+   for _ in range (_AVG_NUM):
+       avg += apin()
+   avg /= _AVG_NUM
+   return(avg)
+while True:
+    analog_val = reading()
+    print(analog_val)
+    time.sleep(1)
+    pass
+
 
 pwm = PWM(0,frequency = 200)
 pwm_c = pwm.channel(0, pin = 'P11', duty_cycle = 0)
@@ -33,4 +45,7 @@ while True:
          print("Temperature: %d C" % result.temperature)
          print("Humidity: %d %%" % result.humidity)
          time.sleep(0.1)
+
+         
+
   
